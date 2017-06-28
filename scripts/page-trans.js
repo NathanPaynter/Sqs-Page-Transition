5b73e1701f5481c6c442f2c62f6f48909f5b1b3d
NathanPaynter <nathanelpaynter@gmail.com>
2017-06-28 14:45:33 -0600

    Updated transition script to function properly in chrome

--- scripts/page-trans.js
+++ scripts/page-trans.js
@@ -1,8 +1,11 @@
 $( function() {
-  $( 'body' ).css( 'opacity', '1' ); //Show body on load
+  $( 'body' ).css( 'opacity', '1' ); // Show body on load
 } );
 $( 'a[href^="/"]:not([href^="#"]):not([target="_blank"])' ).click( function() {
-  //Fade out body when clicking link
-  //that's not an anchor or opening in new tab
-  $( 'body' ).css( 'opacity', '0' );
+  var url = $( this ).attr( 'href' ); // Get url from the <a> href attribute
+  event.preventDefault(); // Prevent default action (e.g. following the link)
+  $( 'body' ).css( 'opacity', '0' ); // Fade body
+  setTimeout( function() {
+    window.location = url;
+  }, 1200 ); // Redirect to the url after 1200ms
 } );
