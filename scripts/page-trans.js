0a617250fba5a03ce26c6feeafd43c53333544fd
NathanPaynter <nathanelpaynter@gmail.com>
2017-06-29 11:22:21 -0600

    Added variable for delay time

--- scripts/page-trans.js
+++ scripts/page-trans.js
@@ -9,10 +9,12 @@ $( function() {
   $( 'body' ).css( 'opacity', '1' ); // Show body on load
 } );
 $( 'a[href^="/"]:not([href^="#"]):not([target="_blank"])' ).click( function() {
+  var $delayTime = 1200; // EDIT THIS IF WANTING TO INCREASE/DECREASE DELAY BEFORE LINK IS FOLLOWED
+
   var url = $( this ).attr( 'href' ); // Get url from the <a> href attribute
   event.preventDefault(); // Prevent default action (e.g. following the link)
   $( 'body' ).css( 'opacity', '0' ); // Fade body
   setTimeout( function() {
     window.location = url;
-  }, 1200 ); // Redirect to the url after 1200ms
+  }, $delayTime ); // Redirect to the url after 1200ms
 } );
